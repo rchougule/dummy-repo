e4fe2b566042092f1fb6eda11a5407e5e7908305
Rohan Chougule <rohan183chougule@gmail.com>
2020-07-23 21:27:00 +0530

    final commit modified message run command
    this is the description. lets see ata hai kya
    
    removed run in steps section
    
    random commit modified #2
    
    port update
    
    branch to tag
    
    ref
    
    dsf
    
    another comit
    
    random 1
    
    checkout branch
    
    log PATH
    
    randoma
    
    random afgain
    
    trying random commit
    
    random 5
    
    again
    
    another commit
    
    random
    
    jobs for all platforms
    
    trying...
    
    try
    
    random
    
    yo
    
    random random
    
    jinga
    
    a
    
    checking commit message
    
    random 17
    
    random 18
    
    no build name
    
    random
    
    20
    
    21 trying
    
    22 trying
    
    23 trying
    
    test run
    
    multi steps
    
    removed project name
    
    trying first PR run
    
    random commit in the PR
    
    removed push trigger from yml
    
    trying out meta
    
    another push to check meta cases
    
    local testing cases
    
    trying out local args and logging level
    
    some fix
    
    deliberate breaking of ubuntu
    
    trying random local identifier
    
    random commit for latest action run
    
    another random addition
    
    trying local
    
    local args fixes
    
    fix
    
    random commit
    
    random run
    
    random commit again
    
    random commit
    
    another random commit
    
    random
    
    new port
    
    removed gloval
    
    random
    
    npm init
    
    random commit koito
    
    stopping local and trting a new test
    
    run another file
    
    random commit
    
    random commit trying
    
    random again git push trting
    
    random again
    
    some random commit forsometign
    
    random commit
    
    all logs try
    
    trying artifacts...
    
    trying
    
    another random commit to check log file artifacts
    
    windows local on off
    
    testing for no logs
    
    running test again for windows
    
    random
    
    random commit
    
    random again
    
    random 52
    
    random try
    
    random again....
    
    random 56
    
    57 random
    
    random try
    
    random 59
    
    add a random commit
    
    debug: case of not inputting the local identifier
    
    random commit 61
    
    random commit 62
    
    trying with no access key
    
    added access key back
    
    trying for push event
    
    trying new split actions
    
    fix
    
    modified test
    
    fix action path in yml
    
    trying with parent action
    
    for mac and windows as well
    
    testing failure conditions
    
    no access key for ubunut
    
    random local-testing input
    
    without logging level
    
    with random identifier
    
    with some string identifier
    
    some local args
    
    without local args
    
    with local arg and identifier
    
    without local args but identifier
    
    without setting env
    
    without username
    
    without explicit build name
    
    aiwayi message
    
    committing to a PR
    
    another commit
    
    with new action repo
    
    dummy commit in dummy repo
    
    random identifier
    
    stopping without starting binary
    
    stopping twice
    
    double steps
    
    binary check for re-download
    
    random commit
    
    random commit 71
    
    random commit 72 yo
    
    multiple artifacts check
    
    merged to master check
    
    double steps for windows
    
    random commit check
    
    without username
    
    checking local-testing without start
    
    with bstack actions
    
    without string quotes
    
    trying on self hosted
    
    trying again with self hosted
    
    again with self hosted
    
    trying with partial binary delete
    
    self mac
    
    push self hosted mac
    
    runner again mac
    
    random commit for self hosted mac
    
    ubuntu self hosted try
    
    mac again
    
    debug binary cache
    
    random: debug with cache
    
    random trigger
    
    on ubuntu self hosted
    
    ubuntu github
    
    second self hosted ubuntu
    
    random commit man
    
    node version 12
    
    setup node with version
    
    trying on windows
    
    windows again check
    
    windows again for self
    
    windows latest
    
    windows self try again
    
    random self windows
    
    self hosted win 3
    
    random win
    
    debug win
    
    binary deletion
    
    trying again
    
    ghjfkhgkkf
    
    93rd man
    
    fresh winself
    
    ubuntu self final try
    
    windows final try
    
    linux self hosted stuck
    
    random linux stuck
    
    random linux
    
    random linux tantrum
    
    dsfsdfsd
    
    100th man
    
    random again 101
    
    probably permission issue
    
    maybe this time
    
    104th
    
    trigger binary linux try
    
    same try ubuntu
    
    new new ubuntu self
    
    final ubuntu
    
    another test for ubuntu
    
    ubuntu latest try
    
    mac os latest try
    
    winows latest try
    
    windows new final
    
    checking for gw
    
    same...
    
    same again...
    
    mac
    
    debug self hosted path
    
    self hosted windows path
    
    linux ubuntu
    
    on all
    
    self hosted mac
    
    self hosted mac again
    
    matric
    
    matrix os
    
    wrong access key

--- another.js
+++ another.js
@@ -1,25 +1,4 @@
-// random commit #random 109
 var webdriver = require('selenium-webdriver');
-// var http = require('http');
-
-// const options = {
-//   host: 'localhost',
-//   port: 8099,
-//   method: 'GET'
-// }
-
-// const response = [];
-
-// http.request(options, (res) => {
-
-//   res.on('data', (chunk) => {
-//     response.push(chunk);
-//   });
-
-//   res.on('end', () => {
-//     console.log(Buffer.concat(response).toString());
-//   })
-// }).end();
 
 // Input capabilities
 var capabilities = {
@@ -29,12 +8,7 @@ var capabilities = {
   'project': process.env.BROWSERSTACK_PROJECT_NAME,
   'os_version': 'high sierra',
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
-  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
-  'browserstack.local': 'true'
-}
-
-if (process.env.BROWSERSTACK_LOCAL_IDENTIFIER) {
-  capabilities['browserstack.localIdentifier'] = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
 }
 
 var driver = new webdriver.Builder()
