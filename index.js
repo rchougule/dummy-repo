f6171b9ebc28444345ff3dbbee05240a37bdd627
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-04 16:56:14 +0530

    another comit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // random commit #3
+
+console.log("CHECK FOR THE SET ENV VARIABLES");
+console.log(process.env.BROWSERSTACK_USERNAME);
+console.log(process.env.BROWSERSTACK_ACCESS_KEY);
+console.log(process.env.BROWSERSTACK_LOCAL_IDENTIFIER);
+console.log(process.env.BROWSERSTACK_BUILD_NAME);
+console.log(process.env.BROWSERSTACK_PROJECT_NAME);
