91d3ee13377e927205099c48ebb543be57302ccf
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-05 17:39:06 +0530

    test run

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ console.log(process.env.BROWSERSTACK_LOCAL_IDENTIFIER);
 console.log(process.env.BROWSERSTACK_BUILD_NAME);
 console.log(process.env.BROWSERSTACK_PROJECT_NAME);
 console.log(process.env.PATH.split(';')[0]);
+
+var webdriver = require('selenium-webdriver');
+
+// Input capabilities
+var capabilities = {
+  'browserName': 'chrome',
+  'os': 'os x',
+  'build': process.env.BROWSERSTACK_BUILD_NAME,
+  'os_version': 'high sierra',
+  'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
+}
+
+var driver = new webdriver.Builder()
+  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+  .withCapabilities(capabilities)
+  .build();
+
+driver.get('http://www.google.com').then(function () {
+  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function () {
+    driver.getTitle().then(function (title) {
+      console.log(title);
+      driver.quit();
+    });
+  });
+});
