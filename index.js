d1cc9fc406c10a28f8b16a236340fb83aa991672
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-06 19:37:01 +0530

    trying local

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var capabilities = {
   'build': process.env.BROWSERSTACK_BUILD_NAME,
   'project': process.env.BROWSERSTACK_PROJECT_NAME,
   'os_version': 'high sierra',
+  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
 }
@@ -26,11 +27,9 @@ var driver = new webdriver.Builder()
   .withCapabilities(capabilities)
   .build();
 
-driver.get('http://www.google.com').then(function () {
-  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function () {
-    driver.getTitle().then(function (title) {
-      console.log(title);
-      driver.quit();
-    });
+driver.get('http://localhost:45454').then(function () {
+  driver.getTitle().then(function (title) {
+    console.log(title);
+    driver.quit();
   });
 });
