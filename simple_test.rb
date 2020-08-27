41ee3ba708b05e1ee818414aca5e9a75dcecee12
steb <steb@stebs-MacBook-Pro.local>
2020-08-27 16:02:41 +0530

    local github action channges

--- simple_test.rb
+++ simple_test.rb
@@ -24,4 +24,7 @@ element = driver.find_element(:name, 'q')
 element.send_keys "BrowserStack"
 element.submit
 puts driver.title
+sleep 1
+driver.navigate.to "http://localhost:5000"
+puts driver.title
 driver.quit
